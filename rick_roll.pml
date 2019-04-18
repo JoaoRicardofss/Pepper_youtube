5d7673e5eb591444ce41bc1be0d9a4e5baef0b2a
João Ricardo Ferreira Santos Silva <38493668+JoaoRicardofss@users.noreply.github.com>
2019-04-18 16:56:35 +0100

    Add files via upload

---
+++ rick_roll.pml
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="rick_roll" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="rick_roll" src="html/rick_roll.mp4" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>OK!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">OK!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK we will not go to youtube!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK we will not go to youtube!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>ihihihihih got you!!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ihihihihih got you!!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I got a better idea youtube is a waste of time! see if you like it</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I got a better idea youtube is a waste of time! see if you like it</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>do you really want me to go to youtube?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">do you really want me to go to youtube?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>do you really want me to go to youtube</source>
+            <comment>Text</comment>
+            <translation type="unfinished">do you really want me to go to youtube</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>great, feel free to touch my tralalala.    hmm, I mean, my tablet!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">great, feel free to touch my tralalala.    hmm, I mean, my tablet!</translation>
+        </message>
+        <message>
+            <source>great, feel free to touch my tra la la la.    hmm, I mean, my tablet!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">great, feel free to touch my tra la la la.    hmm, I mean, my tablet!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>great, feel free to touch my tra la la</source>
+            <comment>Text</comment>
+            <translation type="unfinished">great, feel free to touch my tra la la</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I mean, my tablet!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I mean, my tablet!</translation>
+        </message>
+    </context>
+</TS>
